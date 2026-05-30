62d14e7ada8ae2fa2eae9b8725211fbb48c23d5a
mateomesias2007-oss <mateo.mesias.2007@gmail.com>
2026-05-30 12:48:10 -0300

    Add initial functions for main menu and cartel

---
+++ TP.py
@@ -0,0 +1,10 @@
+def cartel():
+    print("...en construcción")
+
+def MENU():
+    print("MENU PRINCIPAL")
+    print("A - Mayor o Menor")
+    print("B - Numero Secreto")
+    print("C - BlackJack Simple")
+    print("D - Dados. - Par o impar")
+    print("E - Reporte")
