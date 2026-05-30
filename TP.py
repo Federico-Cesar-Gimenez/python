6c540e9f37d303879ddd162a43681e8f56e31696
mateomesias2007-oss <mateo.mesias.2007@gmail.com>
2026-05-30 13:04:42 -0300

    Update TP.py

--- TP.py
+++ TP.py
@@ -8,3 +8,18 @@ def MENU():
     print("C - BlackJack Simple")
     print("D - Dados. - Par o impar")
     print("E - Reporte")
+    print("S- Fin DEL PROGRAMA")
+# acá comienza el Programa Principal
+opc = " " # así lo obligo a entrar al mientras y lo convierto en un Repetir
+while (opc!="S"):
+    MENU()
+    opc = str(input("Ingrese su opcion: "))
+    while (opc<"A" or opc>"E" and opc!= "S"):
+        opc = str(input("Ingreso Invalido - reintente "))
+    match opc:
+        case "A": Juego1
+        case "B": Juego2
+        case "C": cartel()#blackjack
+        case "D": Juego4
+        case "E": cartel()#reporte
+        case "S": print('\n\n GRACIAS POR USAR NUESTRO SISTEMA!!!!')
